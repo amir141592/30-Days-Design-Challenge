bcdcf322bcc0df5c394873d2f411fcdce5fee7c4
Amir Allahdadian <amir.allahdadian@gmail.com>
2023-12-10 01:00:08 +0330

    finished project website with dark mode

---
+++ projects/website-with-dark-mode/website-with-dark-mode.js
@@ -0,0 +1,4 @@
+const container = document.querySelector(".container");
+const btn = document.querySelector(".form-wrapper-left button");
+
+btn.addEventListener("click", () => container.classList.toggle("switch"));
